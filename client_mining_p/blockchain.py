9c24fa1c86ac044f3e56eb471c3009d29897c6d7
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-10-30 15:48:57 +0100

    change last block endpoint

--- client_mining_p/blockchain.py
+++ client_mining_p/blockchain.py
@@ -151,7 +151,7 @@ def full_chain():
 @app.route('/last_block', methods=['GET'])
 def last_block():
     response = {
-        "last_block" : blockchain.chain[-1]
+        "last_block" : blockchain.last_block
     }
     return jsonify(response), 200
 
